887f66ec1d6222175a2769564476b014e3ea59aa
Michael Maier <MichaelMaier225@gmail.com>
2024-03-30 13:24:14 -0500

    Config

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,3 @@
-# pages/views.py
-from django.views.generic import TemplateView
+from django.shortcuts import render
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
-
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
+# Create your views here.
